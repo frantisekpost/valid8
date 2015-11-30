33fb3bbe49d97bda1b4b3626d4b29d4bb4a530de
Frantisek Post <frantisek.post@gmail.com>
2015-11-30 10:16:17 +0100

    more annotations

---
+++ src/main/java/com/frantisekpost/lib/valid8/core/annotations/Custom.java
@@ -0,0 +1,17 @@
+package com.frantisekpost.lib.valid8.core.annotations;
+
+import com.frantisekpost.lib.valid8.core.Logic;
+import com.frantisekpost.lib.valid8.internal.Data;
+import com.frantisekpost.lib.valid8.internal.Trigger;
+
+public @interface Custom {
+
+	Class<? extends Logic> logicClass();
+
+	Class<? extends Data> dataClass();
+
+	String message() default "";
+
+	Class<? extends Trigger> trigger();
+
+}
