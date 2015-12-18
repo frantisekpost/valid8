cf56c9fb3013fb507bfc19b625b5801216ec3965
Frantisek Post <frantisek.post@gmail.com>
2015-12-18 14:22:24 +0100

    public method getComponent

--- src/main/java/com/frantisekpost/lib/valid8/core/impl/ComponentValidator.java
+++ src/main/java/com/frantisekpost/lib/valid8/core/impl/ComponentValidator.java
@@ -5,21 +5,21 @@ import javax.swing.JComponent;
 public class ComponentValidator<T, U extends JComponent> extends AbstractDataValidator<T, U>{
 
 	private U component;
-	
+
 	public ComponentValidator(U component) {
 		if (component == null) {
 			throw new NullPointerException("component cannot be null");
 		}
 		this.component = component;
 	}
-	
+
 	@Override
 	protected boolean canValidate() {
 		return component.isEnabled() && component.isVisible();
 	}
 
 	@Override
-	protected U getComponent() {
+	public U getComponent() {
 		return component;
 	}
 
